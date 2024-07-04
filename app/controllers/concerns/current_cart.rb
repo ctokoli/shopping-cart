6f2789c057551ce3031497db8ac0c76bb1185569
unknown <matumasie@gmail.com>
2024-07-04 22:53:33 +0000

    Update current_cart.rb

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,11 +3,8 @@ module CurrentCart
 
   def set_cart
     @cart = Cart.find(session[:cart_id])
-    puts @cart.inspect
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
     session[:cart_id] = @cart.id
-    puts session[:cart_id].inspect
-    puts @cart.inspect
   end
 end
